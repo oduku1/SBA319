be5cd99e542e7e3aa062b90c9991d264647169d2
Omar Dukureh <132103818+oduku1@users.noreply.github.com>
2025-10-20 21:46:26 -0400

    seeding

--- seedAnime.js
+++ seedAnime.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import User from "./models/User.js"; // your user schema
-import Anime from "./models/Anime.js"; // your anime schema
+import User from "./models/user.js"; 
+import Anime from "./models/anime.js"; 
 
 dotenv.config();
 
@@ -13,16 +13,18 @@ const seed = async () => {
     });
     console.log("Connected to MongoDB");
 
+    // Clear existing data
     await User.deleteMany({});
     await Anime.deleteMany({});
 
-    // Create users
+    // Insert users
     const users = await User.insertMany([
       { username: "1", email: "1@1.com", password: "1" },
       { username: "2", email: "2@2.com", password: "2" },
+      { username: "BobTheCar", email: "Bob@gmail.com", password: "Bob123" }
     ]);
 
-    // Example anime data with genres
+    // Insert anime
     const animes = [
       {
         title: "Chainsaw Man Movie: Reze-hen",
@@ -54,11 +56,63 @@ const seed = async () => {
         image: "https://cdn.myanimelist.net/images/anime/10/47347.jpg",
         userId: users[1]._id,
       },
+
+      // Bob's anime
+      {
+        title: "One Piece",
+        genre: ["Action", "Adventure", "Comedy"],
+        episodes: 1050,
+        rating: 0,
+        status: "Watching",
+        episodesWatched: 500,
+        image: "https://cdn.myanimelist.net/images/anime/6/73245.jpg",
+        userId: users[2]._id,
+      },
+      {
+        title: "My Hero Academia",
+        genre: ["Action", "Comedy", "Superhero"],
+        episodes: 88,
+        rating: 0,
+        status: "Plan to Watch",
+        episodesWatched: 0,
+        image: "https://cdn.myanimelist.net/images/anime/10/78745.jpg",
+        userId: users[2]._id,
+      },
+      {
+        title: "Jujutsu Kaisen",
+        genre: ["Action", "Supernatural", "Fantasy"],
+        episodes: 24,
+        rating: 0,
+        status: "Plan to Watch",
+        episodesWatched: 0,
+        image: "https://cdn.myanimelist.net/images/anime/1184/109114.jpg",
+        userId: users[2]._id,
+      },
+      {
+        title: "Tokyo Revengers",
+        genre: ["Action", "Drama", "Romance"],
+        episodes: 24,
+        rating: 0,
+        status: "Plan to Watch",
+        episodesWatched: 0,
+        image: "https://cdn.myanimelist.net/images/anime/1653/108111.jpg",
+        userId: users[2]._id,
+      },
+      {
+        title: "Spy x Family",
+        genre: ["Action", "Comedy", "Slice of Life"],
+        episodes: 12,
+        rating: 0,
+        status: "Plan to Watch",
+        episodesWatched: 0,
+        image: "https://cdn.myanimelist.net/images/anime/1336/124223.jpg",
+        userId: users[2]._id,
+      }
     ];
 
     await Anime.insertMany(animes);
 
-    console.log("Seed data inserted!");
+    console.log("Seed data for users and anime inserted!");
     process.exit();
   } catch (err) {
     console.error(err);
